979695c720d9f550759a04d649ff2e2d8684a941
ekn1000 <edgarkn1999@gmail.com>
2023-03-12 22:00:47 +0400

    starting

---
+++ grid.js
@@ -0,0 +1,45 @@
+import { Cell } from "./cell.js"
+
+const GRID_SIZE = 4
+const GRID_COUNT = GRID_SIZE * GRID_SIZE
+
+export class Grid {
+    constructor(gridElement) {
+        this.cells = []
+        for (let i = 0; i < GRID_COUNT; i++) {
+            this.cells.push(
+                new Cell(gridElement, i % GRID_SIZE, Math.floor(i / GRID_SIZE))
+            )
+        }
+
+        this.cellsGroupedByColumn = this.groupCellsByColumn()
+        this.cellsGroupedByReversedColumn = this.cellsGroupedByColumn.map(column => [...column].reverse())
+        this.cellsGroupedByRow = this.groupCellsByRow()
+        this.cellsGroupedByReversedRow = this.cellsGroupedByRow.map(row => [...row].reverse())
+    }
+
+
+    getRandomEmptyCell() {
+        const emptyCells = this.cells.filter(cell => cell.isEmpty())
+        const randomIndex = Math.floor(Math.random() * emptyCells.length)
+        return emptyCells[randomIndex]
+    }
+
+    groupCellsByColumn() {
+        return this.cells.reduce((aggr, cell) => {
+            // aggr[cell.x] = aggr[cell.x] || []
+            aggr[cell.x] ||= []
+            aggr[cell.x][cell.y] = cell
+            return aggr
+        }, [])
+    }
+
+    groupCellsByRow() {
+        return this.cells.reduce((aggr, cell) => {
+            // aggr[cell.x] = aggr[cell.x] || []
+            aggr[cell.y] ||= []
+            aggr[cell.y][cell.x] = cell
+            return aggr
+        }, [])
+    }
+}
